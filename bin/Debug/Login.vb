10545d0efb34e635cafc54da5db8667f87d4e986
Indiemandeceased <indieman@outlook.com>
2017-09-25 16:23:31 +0100

    Add files via upload

---
+++ bin/Debug/Login.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+
+Public Class Login
+    Private Sub Login_Load()
+        txtDate.Text = Convert.ToDateTime(Date.Now)
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs)
+        Me.Close()
+        Welcome.Show()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs)
+        txtUsername.Text = ""
+        txtPasswd.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click_1(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+        Welcome.Show()
+    End Sub
+End Class
